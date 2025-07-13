afddbfcb6ef738048d1abd022fdb4e13ab100bad
gengsa <342796923@qq.com>
2025-07-14 00:24:14 +0800

    init project

---
+++ src/main/java/tech/gengsa/testrediscache/TestRedisCacheApplication.java
@@ -0,0 +1,13 @@
+package tech.gengsa.testrediscache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRedisCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRedisCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/gengsa/test_redis_cache/TestRedisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.gengsa.test_redis_cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRedisCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
